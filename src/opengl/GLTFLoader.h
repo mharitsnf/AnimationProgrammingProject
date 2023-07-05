fd55fbba67d8521b5ae923895d0d93c5d6d75984
Harits <mharitsnf@gmail.com>
2023-07-05 15:27:41 +0200

    add pose and clip class

--- src/opengl/GLTFLoader.h
+++ src/opengl/GLTFLoader.h
@@ -2,8 +2,16 @@
 #define _H_GLTFLOADER_
 
 #include "../../external/cgltf/cgltf.h"
+#include "../animation/Pose.h"
+#include "../animation/Clip.h"
+#include <vector>
+#include <string>
 
 cgltf_data* LoadGLTFFile(const char* path);
 void FreeGLTFFile(cgltf_data* handle);
+
+Pose LoadRestPose(cgltf_data* data);
+std::vector<std::string> LoadJointNames(cgltf_data* data);
+std::vector<Clip> LoadAnimationClips(cgltf_data* data);
 
 #endif
